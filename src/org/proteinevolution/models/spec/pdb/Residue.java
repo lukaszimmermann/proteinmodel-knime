9482b38cc5bfe2ec18cd8d58a4bb56bfa73901ac
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-05-08 10:45:17 +0200

    [INTERNAL] Add sidechain and backbone definition to Residue specification

--- src/org/proteinevolution/models/spec/pdb/Residue.java
+++ src/org/proteinevolution/models/spec/pdb/Residue.java
@@ -1,6 +1,8 @@
 package org.proteinevolution.models.spec.pdb;
 
 
+import java.util.Arrays;
+
 import javax.swing.Icon;
 import org.knime.core.node.util.StringIconOption;
 
@@ -12,26 +14,26 @@ import org.knime.core.node.util.StringIconOption;
 public enum Residue implements StringIconOption {
 	
 	// Amino acids
-	ALA,
-	ARG,
-	ASN,
-	ASP,
-	CYS,
-	GLN,
-	GLU,
+	ALA(Atom.CB),
+	ARG(Atom.CB, Atom.CG, Atom.CD, Atom.NE, Atom.CZ, Atom.NH1, Atom.NH2),
+	ASN(Atom.CB, Atom.CG, Atom.OD1, Atom.ND2),
+	ASP(Atom.CB, Atom.CG, Atom.OD1, Atom.OD2),
+	CYS(Atom.CB, Atom.SG),
+	GLN(Atom.CB, Atom.CG, Atom.CD, Atom.OE1, Atom.NE2),
+	GLU(Atom.CB, Atom.CG, Atom.CD, Atom.OE1, Atom.OE2),
 	GLY,
-	HIS,
-	ILE,
-	LEU,
-	LYS,
-	MET,
-	PHE,
-	PRO,
-	SER,
-	THR,
-	TRP,
-	TYR,
-	VAL,
+	HIS(Atom.CB, Atom.CG,  Atom.ND1, Atom.CD2, Atom.CE1, Atom.NE2),
+	ILE(Atom.CB, Atom.CG1, Atom.CG2, Atom.CD1),
+	LEU(Atom.CB, Atom.CG,  Atom.CD1, Atom.CD2),
+	LYS(Atom.CB, Atom.CG, Atom.CD, Atom.CE, Atom.NZ),
+	MET(Atom.CB, Atom.CG, Atom.SD, Atom.CE),
+	PHE(Atom.CB, Atom.CG, Atom.CD1, Atom.CD2, Atom.CE1, Atom.CE2, Atom.CZ),
+	PRO(Atom.CB, Atom.CG, Atom.CD),
+	SER(Atom.CB, Atom.OG),
+	THR(Atom.CB, Atom.OG1, Atom.CG2),
+	TRP(Atom.CB, Atom.CG, Atom.CD1, Atom.CD2, Atom.NE1, Atom.CE2, Atom.CE3, Atom.CZ2, Atom.CZ3, Atom.CH2),
+	TYR(Atom.CB, Atom.CG, Atom.CD1, Atom.CD2, Atom.CE1, Atom.CE2, Atom.CZ, Atom.OH),
+	VAL(Atom.CB, Atom.CG1, Atom.CG2),
 
 	// Nucleotides
 	U,
@@ -39,7 +41,6 @@ public enum Residue implements StringIconOption {
 	C,
 	A,
 	
-	// Misc
 	DT,
 	DA,
 	DG,
@@ -48,6 +49,31 @@ public enum Residue implements StringIconOption {
 	
 	// Unknown
 	UNK;
+	
+	
+	public static Atom[] getBackbone() {
+		
+		return new Atom[] {
+			Atom.N,
+			Atom.CA,
+			Atom.C,
+			Atom.O
+		};
+	}
+	
+	
+	private final Atom[] sidechain;
+	
+	private Residue(Atom... sideChainAtoms) {
+		
+		this.sidechain = sideChainAtoms;
+	}
+	
+
+	public Atom[] getSidechain() {
+		return Arrays.copyOf(this.sidechain, this.sidechain.length);
+	}
+	
 
 	@Override
 	public String getText() {
