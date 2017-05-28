e3522f68f890230695a79ce8b3bd44ff57af867e
lukas <luk.zim91@gmail.com>
2017-05-28 22:37:20 +0200

    [DOC] Improve documentation of Writeable interface

--- src/org/proteinevolution/models/interfaces/Writeable.java
+++ src/org/proteinevolution/models/interfaces/Writeable.java
@@ -4,10 +4,16 @@ import java.io.IOException;
 import java.io.Writer;
 
 /** 
- * Classes implementing this interface represent formats which can be written in plain text to a file
+ * Classes implementing this interface can write their encompassed content to a writer, which might
+ * subsequently used as input to another program.
  * 
- * @author lzimmermann
- *
+ * Points to consider in the future:
+ * <ul>
+ * <li> Should setting the write mode (e.g. FASTA, CLUSTAL for sequence alignments) also be incorporated?</li>
+ * <li> Is the <code>Writer</code> class the correct argument type for the <code>write</code> method?</li>
+ * </ul>
+ * 
+ * @author Lukas Zimmermann
  */
 public interface Writeable {
 
