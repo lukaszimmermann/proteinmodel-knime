ac78dd1b785422a7c0148076e6b76e9ce4af0173
lukas <luk.zim91@gmail.com>
2017-06-07 17:56:59 +0200

    [INTERNAL,REFACTOR] Add OLC to Residue class

--- src/org/proteinevolution/models/spec/pdb/Residue.java
+++ src/org/proteinevolution/models/spec/pdb/Residue.java
@@ -2,53 +2,53 @@ package org.proteinevolution.models.spec.pdb;
 
 
 import java.util.Arrays;
-
 import javax.swing.Icon;
 import org.knime.core.node.util.StringIconOption;
 
 /**
- * Contains specifications for the residues in a PDB structure
+ * Contains specifications for the residues in a PDB structure.
+ * 
  * @author lzimmermann
  *
  */
 public enum Residue implements StringIconOption {
 	
 	// Amino acids
-	ALA(PDBAtom.CB),
-	ARG(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.NE, PDBAtom.CZ, PDBAtom.NH1, PDBAtom.NH2),
-	ASN(PDBAtom.CB, PDBAtom.CG, PDBAtom.OD1, PDBAtom.ND2),
-	ASP(PDBAtom.CB, PDBAtom.CG, PDBAtom.OD1, PDBAtom.OD2),
-	CYS(PDBAtom.CB, PDBAtom.SG),
-	GLN(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.OE1, PDBAtom.NE2),
-	GLU(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.OE1, PDBAtom.OE2),
-	GLY,
-	HIS(PDBAtom.CB, PDBAtom.CG,  PDBAtom.ND1, PDBAtom.CD2, PDBAtom.CE1, PDBAtom.NE2),
-	ILE(PDBAtom.CB, PDBAtom.CG1, PDBAtom.CG2, PDBAtom.CD1),
-	LEU(PDBAtom.CB, PDBAtom.CG,  PDBAtom.CD1, PDBAtom.CD2),
-	LYS(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.CE, PDBAtom.NZ),
-	MET(PDBAtom.CB, PDBAtom.CG, PDBAtom.SD, PDBAtom.CE),
-	PHE(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD1, PDBAtom.CD2, PDBAtom.CE1, PDBAtom.CE2, PDBAtom.CZ),
-	PRO(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD),
-	SER(PDBAtom.CB, PDBAtom.OG),
-	THR(PDBAtom.CB, PDBAtom.OG1, PDBAtom.CG2),
-	TRP(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD1, PDBAtom.CD2, PDBAtom.NE1, PDBAtom.CE2, PDBAtom.CE3, PDBAtom.CZ2, PDBAtom.CZ3, PDBAtom.CH2),
-	TYR(PDBAtom.CB, PDBAtom.CG, PDBAtom.CD1, PDBAtom.CD2, PDBAtom.CE1, PDBAtom.CE2, PDBAtom.CZ, PDBAtom.OH),
-	VAL(PDBAtom.CB, PDBAtom.CG1, PDBAtom.CG2),
+	ALA('A', PDBAtom.CB),
+	ARG('R', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.NE, PDBAtom.CZ, PDBAtom.NH1, PDBAtom.NH2),
+	ASN('N', PDBAtom.CB, PDBAtom.CG, PDBAtom.OD1, PDBAtom.ND2),
+	ASP('D', PDBAtom.CB, PDBAtom.CG, PDBAtom.OD1, PDBAtom.OD2),
+	CYS('C', PDBAtom.CB, PDBAtom.SG),
+	GLN('Q', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.OE1, PDBAtom.NE2),
+	GLU('E', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.OE1, PDBAtom.OE2),
+	GLY('G'),
+	HIS('H', PDBAtom.CB, PDBAtom.CG,  PDBAtom.ND1, PDBAtom.CD2, PDBAtom.CE1, PDBAtom.NE2),
+	ILE('I', PDBAtom.CB, PDBAtom.CG1, PDBAtom.CG2, PDBAtom.CD1),
+	LEU('L', PDBAtom.CB, PDBAtom.CG,  PDBAtom.CD1, PDBAtom.CD2),
+	LYS('K', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD, PDBAtom.CE, PDBAtom.NZ),
+	MET('M', PDBAtom.CB, PDBAtom.CG, PDBAtom.SD, PDBAtom.CE),
+	PHE('F', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD1, PDBAtom.CD2, PDBAtom.CE1, PDBAtom.CE2, PDBAtom.CZ),
+	PRO('P', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD),
+	SER('S', PDBAtom.CB, PDBAtom.OG),
+	THR('T', PDBAtom.CB, PDBAtom.OG1, PDBAtom.CG2),
+	TRP('W', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD1, PDBAtom.CD2, PDBAtom.NE1, PDBAtom.CE2, PDBAtom.CE3, PDBAtom.CZ2, PDBAtom.CZ3, PDBAtom.CH2),
+	TYR('Y', PDBAtom.CB, PDBAtom.CG, PDBAtom.CD1, PDBAtom.CD2, PDBAtom.CE1, PDBAtom.CE2, PDBAtom.CZ, PDBAtom.OH),
+	VAL('V', PDBAtom.CB, PDBAtom.CG1, PDBAtom.CG2),
 
 	// Nucleotides
-	U,
-	G,
-	C,
-	A,
+	U('U'),
+	G('G'),
+	C('C'),
+	A('A'),
 	
-	DT,
-	DA,
-	DG,
-	DC,
-	DU,
+	DT('T'),
+	DA('A'),
+	DG('G'),
+	DC('C'),
+	DU('U'),
 	
 	// Unknown
-	UNK;
+	UNK('U');
 	
 	
 	public static PDBAtom[] getBackbone() {
@@ -61,14 +61,19 @@ public enum Residue implements StringIconOption {
 		};
 	}
 	
-	
 	private final PDBAtom[] sidechain;
-	
-	private Residue(PDBAtom... sideChainAtoms) {
+	private final Character olc;
+		
+	private Residue(final Character olc, final PDBAtom... sideChainAtoms) {
 		
 		this.sidechain = sideChainAtoms;
+		this.olc = olc;
 	}
 	
+	public Character getOLC() {
+		
+		return this.olc;
+	}
 
 	public PDBAtom[] getSidechain() {
 		return Arrays.copyOf(this.sidechain, this.sidechain.length);
