eab02c4061f7850f75474d673dc71bea54087447
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-06-22 21:04:07 +0200

    [ADD] RosettaAbInitioRelaxInvocation stub

---
+++ src/org/proteinevolution/externaltools/RosettaAbInitioRelaxInvocation.java
@@ -0,0 +1,17 @@
+package org.proteinevolution.externaltools;
+
+import java.io.File;
+
+public final class RosettaAbInitioRelaxInvocation extends ExternalToolInvocation<File>{
+
+	public RosettaAbInitioRelaxInvocation(File executable) {
+		super(executable);
+	}
+
+	@Override
+	public File getResult() {
+		
+		// TODO Implement me
+		return null;
+	}
+}
